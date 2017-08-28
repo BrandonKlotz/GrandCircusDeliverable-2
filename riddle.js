68bc9fd79b0ca62bcb1a4c9d0ec34828974f8baa
“BrandonKlotz“ <brandonklotz@icloud.com>
2017-08-28 16:03:04 -0400

    Riddle Init

---
+++ riddle.js
@@ -0,0 +1,17 @@
+var userAnswer = "";
+var riddleAnswer = "Your name"
+var intense = "!!!!"
+
+
+function runRiddle() {
+  if {
+
+  }
+  else {
+
+  }
+  else {
+    document.getElementById('riddleAnswer.innerHTML' = null)
+
+  }
+}
