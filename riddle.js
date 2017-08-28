50a636a8bd57cf914ab925bff2b7c14840ff1263
“BrandonKlotz“ <brandonklotz@icloud.com>
2017-08-28 16:25:09 -0400

    Riddle

--- riddle.js
+++ riddle.js
@@ -1,17 +1,16 @@
-var userAnswer = "";
-var riddleAnswer = "Your name"
+var correctAnswer = "Your name"
 var intense = "!!!!"
 
 
 function runRiddle() {
-  if {
-
+  if (document.getElementById('riddleAnswer').innerHTML('correctAnswer') == true) {
+    document.alert("You got it right" + 'intense');
   }
-  else {
-
+  else if (document.getElementById('riddleAnswer').innerHTML('correctAnswer') == false) {
+    document.alert("Try again" + 'intense');
   }
-  else {
-    document.getElementById('riddleAnswer.innerHTML' = null)
-
+  else (document.getElementById('riddleAnswer').innerHTML('null') == true) {
+    document.alert("Hey there was nothing there" + 'intense');
+    return;
   }
 }
