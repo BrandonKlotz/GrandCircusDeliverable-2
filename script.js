a843a598c7f07ef8a7615c3c0ab9cc51567469d0
“BrandonKlotz“ <brandonklotz@icloud.com>
2017-08-30 10:59:31 -0400

    Update - Issue is prompt returning "Null"

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var correctAnswer = "your name";
-var button = document.getElementsByTagName('button');
 var userInput = document.querySelector('prompt');
 
 function riddle() {
-  var para = document.getElementById('answer');
+  var para = document.getElementById('button');
   para.addEventListener('click', riddle);
   prompt('What is the answer?');
-  if (userInput = correctAnswer){
-    alert(prompt.value + ' is right! GOOD Job!');}
+  if (userInput == correctAnswer){
+    alert(userInput + ' is right! GOOD Job!');}
   else {
-    alert(prompt.value + ' is a good guess! Try again!!');}
+    alert(userInput + ' is a good guess! Try again!!');}
 }
