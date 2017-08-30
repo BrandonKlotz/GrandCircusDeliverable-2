238e45f6d4517ed9b3787ab1f648ea2bfdd80f99
“BrandonKlotz“ <brandonklotz@icloud.com>
2017-08-30 12:12:18 -0400

    Updated Riddle
    And Hey! It is working! :)

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-var correctAnswer = "your name";
-var button = document.getElementsByTagName('button');
-
-
 function riddle() {
   var text;
+  var correctAnswer = "your name";
+    var correctAnswerTwo = "Your name";
   var userInput = prompt('What is the answer?');
   switch (userInput) {
-    case "your name":
+    case correctAnswer:
       text = userInput + ' is right! GOOD Job!';
       break;
+      case correctAnswerTwo:
+        text = userInput + ' is right! GOOD Job!';
+        break;
     default:
       text = userInput + ' is not correct, try again..';
       break;
   }
   document.getElementById("responder").innerHTML = text;
+  return;
 }
